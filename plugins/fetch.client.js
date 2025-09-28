4bd204a8602298d18a0d0a5dcef0599883f63e02
ymslark <ortakrenz@gmail.com>
2025-09-29 03:26:44 +0700

    ganti fetch jadi proccess env

--- plugins/fetch.client.js
+++ plugins/fetch.client.js
@@ -3,9 +3,8 @@ import auth from '@/middleware/auth'
 
 // import dotenv from 'dotenv'
 // env = dotenv.config()
-const config = useRuntimeConfig()
 export default defineNuxtPlugin(nuxtApp => {
-  const BASE_URL =  config.public.apiUrl // Ganti dengan API base URL-mu
+  const BASE_URL =  process.env.API_BASE_URL // Ganti dengan API base URL-mu
   const authStore = useAuthStore()
   // console.log(authStore.isLoggedIn, authStore.accessToken)
   // Fungsi dasar untuk meng-handle request
